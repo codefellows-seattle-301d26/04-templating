20d0ddf77f61177213923597c309a93af59d1312
Jordan <JordanLesaca@Gmail.Com>
2017-12-08 10:18:38 -0800

    converted all article view functions to arrow functions

--- steve-jordan/README.md
+++ steve-jordan/README.md
@@ -12,7 +12,7 @@
 ## Change Log
 Fri Dec 8 09:38:27 2017 -0800: Wrote the handlebars template script in index.html.
 Fri Dec 8 09:56:43 2017 -0800: Edited the article constructor to use the HandleBars template to create a new article and paste it into the HTML.
-
+Fri Dec 8 10:10:48 2017 -0800: fixed a bug in the index.html
 
 ## Credits and Collaborations
 -[jQuery](www.jquery.com)
--- steve-jordan/scripts/article.js
+++ steve-jordan/scripts/article.js
@@ -42,7 +42,7 @@ Article.prototype.toHtml = function() {
 };
 
 // COMMENT: Why are there parentheses around "(a,b)" in the .sort() method, but not around the "articleObject" or "article" arguments in the .forEach() methods?
-// PUT YOUR RESPONSE HERE
+// You have to put parentheses around the input arguments to the arrow function if there are more than one.
 rawData.sort((a,b) => {
   return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
 });
